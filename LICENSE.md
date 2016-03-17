70f78e3d11ec9d60e7ac7f253923893c30135945
Vasili Sviridov <vasili@sviridov.ca>
2016-03-16 21:45:04 -0700

    Deployment schnitzels

---
+++ LICENSE.md
@@ -0,0 +1,1 @@
+Copyright 2016 Charit.ee Inc. All rights reserved.
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./build/Release/rng');
+var rng = require('./build/Release/rng');
+
+if(!rng.isAvailable())
+{
+  throw new Error("Hardware RNG is not available");
+}
+
+module.exports = rng;
